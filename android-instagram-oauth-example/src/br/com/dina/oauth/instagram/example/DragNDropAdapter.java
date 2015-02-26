29327a05ef305b6765add39fc0be142f159a8634
chandrika33 <chandrika.darapaneni@gmail.com>
2015-02-25 16:54:25 -0800

    instagram_with_drag_n_drop_fix
    Using the Instagram API, create an Android app  that displays photos
    tagged with hashtag #selfie and arranges them using the pattern: big,
    small, small, big, small small, and repeat. Then implemented the
    following features: Tap to enlarge, Drag and drop reordering, or
    Infinite scrolling.

---
+++ android-instagram-oauth-example/src/br/com/dina/oauth/instagram/example/DragNDropAdapter.java
@@ -0,0 +1,207 @@
+/*
+ * Copyright (C) 2010 Eric Harlow
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.com.dina.oauth.instagram.example;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public final class DragNDropAdapter extends BaseAdapter implements RemoveListener, DropListener , OnClickListener {
+
+	private int[] mIds;
+    private int[] mLayouts;
+    private LayoutInflater mInflater;
+    private ArrayList<String> mContent;
+    private static LayoutInflater inflater = null;
+	public ImageLoader imageLoader;
+    static String TAG = DragNDropAdapter.class.getCanonicalName();
+
+    public DragNDropAdapter(Context context, ArrayList<String> content) {
+        init(context,new int[]{android.R.layout.simple_list_item_1},new int[]{android.R.id.text1}, content);
+    }
+    
+    public DragNDropAdapter(Context context, int[] itemLayouts, int[] itemIDs, ArrayList<String> content) {
+    	init(context,itemLayouts,itemIDs, content);
+    }
+
+    private void init(Context context, int[] layouts, int[] ids, ArrayList<String> content) {
+    	// Cache the LayoutInflate to avoid asking for a new one each time.
+    	mInflater = LayoutInflater.from(context);
+    	mIds = ids;
+    	mLayouts = layouts;
+    	mContent = content;
+    	imageLoader = new ImageLoader(context);
+    	Log.e(TAG, "content 111 ---" + mContent);
+    }
+    
+    /**
+     * The number of items in the list
+     * @see android.widget.ListAdapter#getCount()
+     */
+    public int getCount() {
+        return mContent.size();
+    }
+
+    /**
+     * Since the data comes from an array, just returning the index is
+     * sufficient to get at the data. If we were using a more complex data
+     * structure, we would return whatever object represents one row in the
+     * list.
+     *
+     * @see android.widget.ListAdapter#getItem(int)
+     */
+    public String getItem(int position) {
+        return mContent.get(position);
+    }
+
+    /**
+     * Use the array index as a unique id.
+     * @see android.widget.ListAdapter#getItemId(int)
+     */
+    public long getItemId(int position) {
+        return position;
+    }
+
+    /********* Create a holder Class to contain inflated xml file elements *********/
+	public static class ViewHolder {
+
+		public TextView text;
+		public ImageView image;
+
+	}
+
+	/*public View getView(int position, View convertView, ViewGroup parent) {
+
+		View vi = convertView;
+		ViewHolder holder;
+
+		try {
+			Log.e(TAG, "getView ---" + mContent);
+			if (convertView == null) {
+
+				*//****** Inflate tabitem.xml file for each row ( Defined below ) *******//*
+				vi = inflater.inflate(R.layout.list_row, null);
+
+				*//****** View Holder Object to contain tabitem.xml file elements ******//*
+
+				holder = new ViewHolder();
+				holder.text = (TextView) vi.findViewById(R.id.textView1);
+				holder.image = (ImageView) vi.findViewById(R.id.imageView1);
+
+				*//************ Set holder with LayoutInflater ************//*
+				vi.setTag(holder);
+			} else {
+				holder = (ViewHolder) vi.getTag();
+			}
+
+			ImageView image = holder.image;
+			Log.e(TAG, "content ---" + mContent);
+			// DisplayImage function from ImageLoader Class
+			imageLoader.DisplayImage(mContent.get(position), image);
+
+			*//******** Set Item Click Listner for LayoutInflater for each row ***********//*
+			vi.setOnClickListener(new OnItemClickListener(position));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return vi;
+	}*/
+	
+	 public View getView(int position, View convertView, ViewGroup parent) {
+	        // A ViewHolder keeps references to children views to avoid unneccessary calls
+	        // to findViewById() on each row.
+	        ViewHolder holder;
+
+	        // When convertView is not null, we can reuse it directly, there is no need
+	        // to reinflate it. We only inflate a new View when the convertView supplied
+	        // by ListView is null.
+	        if (convertView == null) {
+	            convertView = mInflater.inflate(mLayouts[0], null);
+
+	            // Creates a ViewHolder and store references to the two children views
+	            // we want to bind data to.
+	            holder = new ViewHolder();
+	            holder.text = (TextView) convertView.findViewById(R.id.textView1);
+	            holder.image = (ImageView) convertView.findViewById(mIds[0]);
+
+	            convertView.setTag(holder);
+	        } else {
+	            // Get the ViewHolder back to get fast access to the TextView
+	            // and the ImageView.
+	            holder = (ViewHolder) convertView.getTag();
+	        }
+	        
+	        ImageView image = holder.image;
+			Log.e(TAG, "content ---" + mContent);
+			// DisplayImage function from ImageLoader Class
+			imageLoader.DisplayImage(mContent.get(position), image);
+
+			/******** Set Item Click Listner for LayoutInflater for each row ***********/
+			image.setOnClickListener(new OnItemClickListener(position));
+	        // Bind the data efficiently with the holder.
+	        holder.text.setText(mContent.get(position));
+	        holder.text.setVisibility(View.INVISIBLE);
+	        return convertView;
+	    }
+
+	@Override
+	public void onClick(View arg0)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/********* Called when Item click in ListView ************/
+	private class OnItemClickListener implements OnClickListener {
+		private int mPosition;
+
+		OnItemClickListener(int position)
+		{
+			mPosition = position;
+		}
+
+		@Override
+		public void onClick(View arg0)
+		{
+			TouchImageView img = (TouchImageView) arg0.findViewById(R.id.imageView1);
+	        img.setMaxZoom(4);
+		}
+	}
+	
+
+	public void onRemove(int which) {
+		Log.e(TAG, "inside onRemove -- " + which + "::" + mContent);
+		if (which < 0 || which > mContent.size()) return;		
+		mContent.remove(which);
+	}
+
+	public void onDrop(int from, int to) {
+		String temp = mContent.get(from);
+		Log.e(TAG, "inside onDrop -- FROM= " + from + ":: TO=" + to + ":: temp" + temp);
+		mContent.remove(from);
+		mContent.add(to,temp);
+	}
+
+}
---
+++ android-instagram-oauth-example/src/br/com/dina/oauth/instagram/example/Home.java
@@ -0,0 +1,139 @@
+package br.com.dina.oauth.instagram.example;
+
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class Home extends ListActivity {
+	static String TAG = Home.class.getCanonicalName();
+
+	ListView m_listView;
+
+//	ListLoader m_listLoader;
+	DragNDropAdapter adapter;
+	
+	String m_strHistory = "";
+	
+	ArrayList<String> imagesData = new ArrayList<String>();
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home);
+
+		Bundle b = getIntent().getExtras();
+		if(b != null)
+		{
+			m_strHistory = b.getString("HISTORY");
+			
+			Log.d("HISTORY", m_strHistory);
+		}
+		
+		try
+		{
+			JSONArray jArray = new JSONArray(m_strHistory);
+			
+			for(int i=0; i<jArray.length(); i++)
+			{
+				JSONObject jObj = jArray.getJSONObject(i);
+				JSONArray jjArray = jObj.getJSONArray("tags");
+				if(jjArray.length() != 0)
+				{
+					JSONObject jj = jObj.getJSONObject("images").getJSONObject("low_resolution");
+					String imageUrlString = jj.getString("url");
+					
+					URL url = new URL(imageUrlString);
+					
+					imagesData.add(url.toString());
+				}
+				
+			}
+			Log.e(TAG , "imagesData --- " + imagesData);
+				
+		} catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+//		m_listView = (ListView) findViewById(R.id.listView1);
+		
+//		m_listLoader = new ListLoader(this, imagesData);
+//		setListAdapter(m_listLoader);
+		
+		adapter = new DragNDropAdapter(this, new int[]{R.layout.list_row}, new int[]{R.id.imageView1}, imagesData);//new DragNDropAdapter(this,content)
+		setListAdapter(adapter);
+		
+		 ListView listView = getListView();
+	        
+	        if (listView instanceof DragNDropListView) {
+	        	((DragNDropListView) listView).setDropListener(mDropListener);
+	        	((DragNDropListView) listView).setRemoveListener(mRemoveListener);
+	        	((DragNDropListView) listView).setDragListener(mDragListener);
+	        }
+	}
+		private DropListener mDropListener = 
+				new DropListener() {
+		        public void onDrop(int from, int to) {
+//		        	Log.e(TAG, "inside mDropListener -- ");
+		        	ListAdapter adapter = getListAdapter();
+		        	if (adapter instanceof DragNDropAdapter) {
+		        		((DragNDropAdapter)adapter).onDrop(from, to);
+		        		getListView().invalidateViews();
+		        	}
+		        }
+		    };
+		    
+		    private RemoveListener mRemoveListener =
+		        new RemoveListener() {
+		        public void onRemove(int which) {
+//		        	Log.e(TAG, "inside mRemoveListener -- ");
+		        	ListAdapter adapter = getListAdapter();
+		        	if (adapter instanceof DragNDropAdapter) {
+		        		((DragNDropAdapter)adapter).onRemove(which);
+		        		getListView().invalidateViews();
+		        	}
+		        }
+		    };
+		    
+		    private DragListener mDragListener =
+		    	new DragListener() {
+
+		    	int backgroundColor = 0xe0103010;
+		    	int defaultBackgroundColor;
+		    	
+					public void onDrag(int x, int y, ListView listView) {
+						// TODO Auto-generated method stub
+//						Log.e(TAG, "inside onDrag -- ");
+					}
+
+					public void onStartDrag(View itemView) {
+//						Log.e(TAG, "inside onStartDrag -- ");
+						itemView.setVisibility(View.INVISIBLE);
+						defaultBackgroundColor = itemView.getDrawingCacheBackgroundColor();
+						itemView.setBackgroundColor(backgroundColor);
+//						ImageView iv = (ImageView)itemView.findViewById(R.id.imageView1);
+//						if (iv != null) iv.setVisibility(View.INVISIBLE);
+					}
+
+					public void onStopDrag(View itemView) {
+//						Log.e(TAG, "inside onStopDrag -- ");
+						itemView.setVisibility(View.VISIBLE);
+						itemView.setBackgroundColor(defaultBackgroundColor);
+						ImageView iv = (ImageView)itemView.findViewById(R.id.imageView1);
+						if (iv != null) iv.setVisibility(View.VISIBLE);
+					}
+		    	
+		    };
+}
