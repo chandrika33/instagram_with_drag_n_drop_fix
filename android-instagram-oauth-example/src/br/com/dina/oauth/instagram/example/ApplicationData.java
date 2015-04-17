977e1b136b191e6cb59cb01c46b5302f4d62022c
chandrika33 <chandrika.darapaneni@gmail.com>
2015-04-17 16:02:59 -0700

    Update ApplicationData.java

--- android-instagram-oauth-example/src/br/com/dina/oauth/instagram/example/ApplicationData.java
+++ android-instagram-oauth-example/src/br/com/dina/oauth/instagram/example/ApplicationData.java
@@ -1,4 +1,4 @@
-package br.com.oauth.instagram.example;
+package br.com.dina.oauth.instagram.example;
 
 public class ApplicationData {
 	public static final String CLIENT_ID = "379d744556c743c090c8a2014779f59f";
